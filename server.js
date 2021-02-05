91a2853d0dafc57327655e9fd79714e8a0a58f51
DolMalin <58857339+DolMalin@users.noreply.github.com>
2021-02-05 19:12:52 +0100

    v1

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const urlencode = require('urlencode')
 const curl = require('curlrequest')
 const glitcher = require('./glitcher')
 require('dotenv').config()
+const shedule = require('node-shedule')
+const { scheduleJob } = require('node-schedule')
 
 var photo = urlencode(process.env.IMAGE_URL);
 var optionImg = {
@@ -9,7 +11,16 @@ var optionImg = {
     method:'POST'
 }
 
-// curl.request(optionImg, (err, data) =>{
-// })
+const sendPost = () => {
+    schedule.scheduleJob('* * 1 * * *', () => {
+        let photo = urlencode(process.env.GLITCHED_IMAGE_URL)
+        let optionImg = {
+            url: `https://graph.facebook.com/${process.env.PAGE_ID}/photos?url=${photo}&access_token=${process.env.FACEBOOK_TOKEN}`,
+            method:'POST'
+        }
+        curl.request(optionImg, (err, data) => {})
+    })
+}
 
-
+glitcher()
+sendPost()
