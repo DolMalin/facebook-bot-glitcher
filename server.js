51788f1e47860b773e492371e2975c941fe38b4d
DolMalin <58857339+DolMalin@users.noreply.github.com>
2021-02-05 23:58:55 +0100

    v1

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var optionImg = {
 }
 
 const sendPost = () => {
-    schedule.scheduleJob('*/29 * * * *', () => {
+    schedule.scheduleJob('*/30 * * * *', () => {
         let photo = urlencode(process.env.GLITCHED_IMAGE_URL)
         let optionImg = {
             url: `https://graph.facebook.com/${process.env.PAGE_ID}/photos?url=${photo}&access_token=${process.env.FACEBOOK_TOKEN}`,
